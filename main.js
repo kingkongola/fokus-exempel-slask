088c80a202b087922c71230896fd7ac3d1cf9b49
Adam Bergman <adam.bergman@arbetsformedlingen.se>
2025-12-11 13:50:37 +0100

    resten

---
+++ main.js
@@ -0,0 +1,5 @@
+import { defineCustomElements as defineDigi } from '@designsystem-se/af/loader';
+import { defineCustomElements as defineYV } from '@taxonomy/yrkesvaljaren/loader';
+
+defineDigi(window);
+defineYV(window);
